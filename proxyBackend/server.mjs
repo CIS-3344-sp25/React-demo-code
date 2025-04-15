d9b23a8374235b0755b6417c278258b8d0d2c9b3
mounikaravi-hospet <mounikaravi.hospet@gmail.com>
2025-04-15 12:33:38 -0400

    port change

--- proxyBackend/server.mjs
+++ proxyBackend/server.mjs
@@ -33,6 +33,6 @@ app.get('/api/articles', async(req, res) => {
     }
 })
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+// app.listen(5000, () => {
+//     console.log('Server is running on port 5000');
+// });
